e8110cce2522fca0e70e40cf70b6434f97b3121d
Nikita <44435714+Nickpicnic@users.noreply.github.com>
2019-09-14 20:11:46 +0300

    0
    node echo.js

---
+++ NodeJS/Basic/echo.js
@@ -0,0 +1,24 @@
+function echo(string, num){
+    for (var i = 0; i < num; i++){
+        console.log(string);
+    }
+};
+
+echo("Echo!!!", 10);
+echo("Tater Tots", 3);
+
+// node echo.js
+//
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Echo!!!
+// Tater Tots
+// Tater Tots
+// Tater Tots

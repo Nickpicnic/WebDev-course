f87979c03cc7f8e9c58b5a45096d1f8e05344c10
Nikita <44435714+Nickpicnic@users.noreply.github.com>
2019-09-28 01:44:46 +0300

    Add files via upload

---
+++ YelpCampProject/v9/middleware/index.js
@@ -0,0 +1,53 @@
+var Campground      = require("../models/campground"),
+    Comment         = require("../models/comment"),
+// all the middleware goes here
+    middlewareObj   = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            if (err) {
+                res.redirect("back");
+            } else {
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "You don't have permission to do that");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+};
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.id, (err, foundComment) => {
+            if (err) {
+                req.flash("error", "Campground not found");
+                res.redirect("back");
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "You don't have permission to do that");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+};
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+};
+
+module.exports = middlewareObj;
---
+++ YelpCampProject/v9/seeds.js
@@ -0,0 +1,62 @@
+var mongoose        = require("mongoose"), 
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    data            = [
+        {
+            name: "Clouds Rest", 
+            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" 
+        },
+        {
+            name: "Desert Mesa", 
+            image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=698&q=80",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        },
+        {
+            name: "Stars Dome", 
+            image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        }
+    ];
+
+function seedDB(){
+    // Remove all campgrounds
+    Campground.remove({}, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        Comment.remove({}, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("removed comments");
+        });
+        // add a few campgrounds
+        data.forEach((seed) => {
+            Campground.create(seed, (err, campground) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Added a Campground!");
+                    // create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great!",
+                            author: "Homer!"
+                        }, (err, comment) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created a new comment!");
+                            }
+                        });
+                }
+            });
+        });
+    });
+}   
+
+module.exports = seedDB;
